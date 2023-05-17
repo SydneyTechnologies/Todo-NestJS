8fbd8ad8704783b03c6ea42c96b735a5381867db
sydneyIdundun <sydney.idundun@gmail.com>
2023-05-17 09:07:02 +0400

    adding documentation for the project

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const apiConfiguration = new DocumentBuilder().setTitle("NestJS Todo Api")
+  .setDescription("This is a simple CRUD application for a todo application written in NestJS")
+  .setVersion("1.0").
+  build()
+
+  const document = SwaggerModule.createDocument(app, apiConfiguration)
+  SwaggerModule.setup("docs", app, document)
   app.useGlobalPipes(new ValidationPipe({transform: true}));
   await app.listen(process.env.PORT || 3000);
 }
