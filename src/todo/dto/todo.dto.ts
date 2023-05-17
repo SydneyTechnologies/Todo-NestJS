95e6f0f940d046ddf720c5f374419557c858c9f6
sydneyIdundun <sydney.idundun@gmail.com>
2023-05-17 09:43:57 +0400

    added the schema definitions to the swagger docs

--- src/todo/dto/todo.dto.ts
+++ src/todo/dto/todo.dto.ts
@@ -1,27 +1,33 @@
 // create the user dtos in this typescript file 
 import { IsString, IsInt, IsDateString, IsBoolean, IsNotEmpty, IsIn, IsOptional, IsDate, isDate } from "class-validator";
 import { Transform, Type} from "class-transformer";
+import { ApiPropertyOptional, ApiProperty } from "@nestjs/swagger";
 
 export class CreateTodoDto{
+    @ApiProperty()
     @IsNotEmpty()
     @IsString()
     title: string;
 
+    @ApiPropertyOptional()
     @Type(()=>Date)
     @IsDate()
     @IsOptional()
     reminder: Date
 
+    @ApiPropertyOptional()
     @IsString()
     @IsOptional()
     description: string;
 
+    @ApiPropertyOptional()
     @IsIn(['PRIORITY_1', 'PRIORITY_2', 'PRIORITY_3'])
     @IsString()
     @IsOptional()
     flag: string;
 
 
+    @ApiPropertyOptional()
     @Type(()=>Boolean)
     @IsBoolean()
     @IsOptional()
@@ -31,6 +37,7 @@ export class CreateTodoDto{
 }
 
 export class UpdateTodoDto extends CreateTodoDto{
+    @ApiPropertyOptional()
     @IsOptional()
     title: string;
 }
